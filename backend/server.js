93b7356c8ae360b80a4a236e32cccd4fe93364f5
eisha-mubashir <eisha3744@gmail.com>
2026-05-12 05:14:04 -0400

    first commit

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,19 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 
 // MongoDB Connection
-mongoose.connect('mongodb://127.0.0.1:27017/demo', {
+ mongoose.connect(process.env.MONGO_URI || 'mongodb://mongo:27017/mywebsite', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-    .then(() => console.log('MongoDB Connected - DB: demo'))
-    .catch(err => console.error('MongoDB Connection Error:', err));
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.error('MongoDB Connection Error:', err));
 
 
 const userSchema = new mongoose.Schema({
